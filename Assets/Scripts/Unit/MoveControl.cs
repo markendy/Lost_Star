842ffb7323fcce1f3367a1fb7113894104872c91
markendy <g.alex.2000@mail.ru>
2020-08-24 01:31:26 +0300

    Added & настроен MoveControl Script. Upped size Hero. Change slow speen 3 to 20 frames.

---
+++ Assets/Scripts/Unit/MoveControl.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class MoveControl : MonoBehaviour
+{
+    public Action JumpHendler;
+    public Action MoveRightHendler;
+    public Action MoveLeftHendler;
+    public Action MoveStopHendler;
+    private Hero hero;
+    private float moveInput = 0f;
+    private bool facingR = true;
+    private bool isGrounded;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float checkRadius;
+    [SerializeField] private LayerMask whatIsGround;
+    private Rigidbody2D rigidbody;
+    private int extraJump;
+    public int extraJumpValue;
+    [SerializeField] private Animator animator;
+    private int isR = 0;
+    private bool isJ = false;
+    private float deltaMove = 0;
+    private void Start()
+    {
+        isR = 0;
+        isJ = false;
+        deltaMove = 0;
+        moveInput = 0f;
+        hero = GetComponent<Hero>();
+        extraJump = extraJumpValue;
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (isR == 1)
+        {
+            RightButtonDown();
+        }
+        if (isR == -1)
+        {
+            LeftButtonDown();
+        }
+        if (isR == 0)
+        {
+            StopButtonDown();
+        }
+        if (!facingR && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (facingR && moveInput < 0)
+        {
+            Flip();
+        }
+        if (moveInput == 0)
+        {
+            animator.SetBool("isRunning", false);
+        }
+        else
+        {
+            animator.SetBool("isRunning", true);
+        }
+    }
+    private void Update()
+    {
+        JumpControl();
+    }
+
+    private void JumpControl()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        if (isGrounded)
+        {
+            animator.SetBool("isJumping", false);
+        }
+        if (isJ)
+        {
+            isJ = false;
+            if (isGrounded)
+            {
+                extraJump = extraJumpValue;
+                rigidbody.velocity = Vector2.up * hero.JumpForce;
+                animator.SetBool("isJumping", true);
+            }
+            else if (!isGrounded && extraJump > 0)
+            {
+                rigidbody.velocity = Vector2.up * hero.JumpForce;
+                --extraJump;
+                animator.SetBool("isJumping", true);
+            }
+        }
+        if (extraJump < 0)
+        {
+            extraJump = 0;
+        }
+    }
+    private void CreateVector(int v)
+    {
+        moveInput = moveInput + 1.5f * v * Time.deltaTime;
+        if (moveInput > 1)
+        {
+            moveInput = 1;
+        }
+        else if (moveInput < -1)
+        {
+            moveInput = -1;
+        }
+    }
+    private void InVector0()
+    {
+        if (moveInput == 0)
+        {
+            deltaMove = 0;
+        }
+        else
+        {
+            if (deltaMove == 0)
+            {
+                deltaMove = Math.Abs(moveInput) / 20f;
+            }
+            if (moveInput < 0)
+            {
+                moveInput += deltaMove;
+                if (moveInput > 0)
+                {
+                    moveInput = 0;
+                }
+            }
+            else if (moveInput > 0)
+            {
+                moveInput -= deltaMove;
+                    if (moveInput < 0)
+                    {
+                        moveInput = 0;
+                    }
+            }
+            CreateVector(0);
+        }
+    }
+    public void DoJump()
+    {
+        isJ = true;
+    }
+    public void EndMove()
+    {
+        isR = 0;
+    }
+    public void MoveLeft()
+    {
+        isR = -1;
+    }
+    public void MoveRight()
+    {
+        isR = 1;
+    }
+    private void StopButtonDown()
+    {
+        InVector0();
+        rigidbody.velocity = new Vector2(moveInput * hero.Speed, rigidbody.velocity.y);
+        MoveStopHendler();
+    }
+    private void LeftButtonDown()
+    {
+        CreateVector(-1);
+        rigidbody.velocity = new Vector2(moveInput * hero.Speed, rigidbody.velocity.y);
+        MoveLeftHendler();
+    }
+    private void RightButtonDown()
+    {
+        CreateVector(1);
+        rigidbody.velocity = new Vector2(moveInput * hero.Speed, rigidbody.velocity.y);
+        MoveRightHendler();
+    }
+    private void Flip()
+    {
+        facingR = !facingR;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+}
---
+++ Assets/Scripts/Unit/Unit.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using LostStar;
+public class Unit : MonoBehaviour, IUnit
+{
+    public double heals = 1;
+    public double Heals
+    {
+        get => heals;
+        set
+        {
+            if (value <= 0){
+                //Deas();
+                heals = value;
+            }
+            else if (value > MaxHeals)
+            {
+                heals = MaxHeals;
+            }
+            else { heals = value; }
+        }
+    }
+    public double maxHeals = 1;
+    public double MaxHeals
+    {
+        get => maxHeals;
+        set
+        {
+            if (maxHeals <= 0) maxHeals = 100;
+            if(maxHeals == heals){
+                maxHeals = value;
+                heals = maxHeals;
+            }
+            else { maxHeals = value; }
+        }
+    }
+    public double regenHeals = 1;
+    public double RegenHeals { get => regenHeals; set { if (regenHeals < 0) regenHeals = 0; else { regenHeals = value; } } }
+    public double damage = 1;
+    public double Damage { get => damage; set { if (damage < 0) damage = 0; else { damage = value; } } }
+    public float speed = 1f;
+    public float Speed { get => speed; set { if (speed <= 0) speed = 0.1f; else { speed = value; } } }
+    public float jumpForce = 1f;
+    public float JumpForce { get => jumpForce; set { if (jumpForce <= 0) jumpForce = 0.1f; else { jumpForce = value; } } }
+    public float attackRange = 1f;
+    public float AttackRange { get => attackRange; set { if (attackRange <= 0) attackRange = 0.1f; else { attackRange = value; } } }
+    public double armor;
+    public double Armor { get => armor; set { armor = value; } }
+    public double physicResist;
+    public double PhysicResist { get => physicResist = (0.05 * Armor) / (1 + 0.05 * Armor); set { physicResist = value; } }
+    public ExpSystem expSystem;
+    public ExpSystem ExpSystem { get => expSystem; set { expSystem = value; } }
+    public double givingExp;
+    public double GivingExp { get => givingExp; set { givingExp = value; } }
+    void FixedUpdate()
+    {
+        RegenHP();
+        PhysicResist = PhysicResist;// UPDATE RESIST
+    }
+    public virtual void Start(){
+        ExpSystem = GetComponent<ExpSystem>();
+        Init();
+    }
+    public virtual void Init()
+    {
+        MaxHeals = 100;
+        Heals = MaxHeals;
+        RegenHeals = 5;
+        Damage = 40;
+        Speed = 5.5f;
+        JumpForce = 4.5f;
+        AttackRange = 0.15f;
+        Armor = 5;
+//        GivingExp = 50*(ExpSystem.lvl+1);
+    }
+    public virtual void Deas()
+    {
+        //Instantiate(gameObject, new Vector3(transform.position.x, transform.position.y+10, 0), transform.rotation);
+        Destroy(gameObject);
+    }
+    public void RegenHP()
+    {
+        Heals += RegenHeals * Time.deltaTime;
+    }
+    public void TakeDamage(double dmg, int type, GameObject ktodmg)
+    {
+        switch (type)
+        {
+            case 1://PHYSICS
+                Heals -= dmg * (1 - PhysicResist);
+                if(Heals < 0){
+                    ktodmg.GetComponent<Unit>().ExpSystem.Exp += GetComponent<Unit>().GivingExp;
+                    Deas();
+                }
+                break;
+            case 2://MAGIC
+                break;
+            default:
+                Heals -= dmg;
+                break;
+        }
+    }
+    public void UpgradeStats(){
+        MaxHeals += 25 * ExpSystem.lvl;
+        Damage *= 1.06;
+        Speed *= 1.005f;
+        RegenHeals *= 1.07;// * expSystem.lvl;
+    }
+    //void OnTriggerEnter
+}
